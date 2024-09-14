f5f7e5d59842d551c02d8f8c1865c98192a94536
Anish Vundela <anishreddyvundela@proton.me>
2024-09-14 10:42:00 +0100

    Add the robot centric mecanum drive to the Solo and Duo TeleOps

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Solo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Solo.java
@@ -4,6 +4,9 @@ import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.common.command.teleop.RobotCentricMecanumDriveCmd;
+import org.firstinspires.ftc.teamcode.common.subsystem.MecanumDriveSubsystem;
+
 @SuppressWarnings({"FieldCanBeLocal", "unused"})
 @TeleOp
 public class Solo extends CommandOpMode {
@@ -14,9 +17,35 @@ public class Solo extends CommandOpMode {
      */
     private GamepadEx gamepadEx;
 
+    /**
+     * The {@link com.arcrobotics.ftclib.command.Subsystem}s that will be used in the TeleOp for the
+     * command based paradigm.
+     */
+    private MecanumDriveSubsystem driveSubsystem;
+
+    /**
+     * The {@link com.arcrobotics.ftclib.command.Command}s that will be used in the TeleOp for the
+     * command based paradigm.
+     */
+    private RobotCentricMecanumDriveCmd driveCmd;
+
     @Override
     public void initialize() {
         // Instantiate the GamepadEx
         gamepadEx = new GamepadEx(gamepad1);
+
+        // Instantiate the Subsystems
+        driveSubsystem = new MecanumDriveSubsystem(hardwareMap);
+
+        // Instantiate the Commands
+        driveCmd = new RobotCentricMecanumDriveCmd(
+                driveSubsystem,
+                gamepadEx::getLeftY,
+                gamepadEx::getLeftX,
+                gamepadEx::getRightX
+        );
+
+        // Set default commands
+        driveSubsystem.setDefaultCommand(driveCmd);
     }
 }
