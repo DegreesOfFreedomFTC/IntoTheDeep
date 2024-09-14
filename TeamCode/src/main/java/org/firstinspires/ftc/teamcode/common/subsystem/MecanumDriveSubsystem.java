1105cfb487536398a7ae826088661a6184fff5fc
Anish Vundela <anishreddyvundela@proton.me>
2024-09-14 10:33:00 +0100

    Add MecanumDriveSubsystem
    The MecanumDriveSubsystem class takes in a HardwareMap and uses it to instantiate the mecanum drivetrain motors. It can then use these in a drive() function that takes in the driveSpeed, strafeSpeed, and turnSpeed and does the maths with them to power the motors.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/MecanumDriveSubsystem.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.common.subsystem;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@SuppressWarnings("unused")
+public class MecanumDriveSubsystem extends SubsystemBase {
+
+    private final DcMotor frontLeftMotor;
+    private final DcMotor frontRightMotor;
+    private final DcMotor backLeftMotor;
+    private final DcMotor backRightMotor;
+
+    public MecanumDriveSubsystem(final HardwareMap hardwareMap) {
+        this.frontLeftMotor = hardwareMap.get(DcMotor.class, "dtFrontLeftMotor");
+        this.frontRightMotor = hardwareMap.get(DcMotor.class, "dtFrontRightMotor");
+        this.backLeftMotor = hardwareMap.get(DcMotor.class, "dtBackLeftMotor");
+        this.backRightMotor = hardwareMap.get(DcMotor.class, "dtBackRightMotor");
+
+        this.frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void drive(double driveSpeed, double strafeSpeed, double turnSpeed) {
+        // Ensure that speeds have the same ratio
+        double denominator = Math.max(
+                Math.abs(driveSpeed) + Math.abs(strafeSpeed) + Math.abs(turnSpeed),
+                1.0
+        );
+
+        double frontLeftSpeed  = (driveSpeed + strafeSpeed + turnSpeed) / denominator;
+        double frontRightSpeed = (driveSpeed - strafeSpeed - turnSpeed) / denominator;
+        double backLeftSpeed   = (driveSpeed - strafeSpeed + turnSpeed) / denominator;
+        double backRightSpeed  = (driveSpeed + strafeSpeed - turnSpeed) / denominator;
+
+        frontLeftMotor.setPower(frontLeftSpeed);
+        frontRightMotor.setPower(frontRightSpeed);
+        backLeftMotor.setPower(backLeftSpeed);
+        backRightMotor.setPower(backRightSpeed);
+    }
+}
