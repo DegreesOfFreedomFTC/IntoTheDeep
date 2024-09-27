9a408f2568e98eda4aed4406bb188048b21c513d
Anish Vundela <anishreddyvundela@proton.me>
2024-09-27 21:19:16 +0100

    Make ClawSubsystem send telemetry
    The ClawSubsystem class will send a single TelemetryLine with the angle of the claw servo.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/ClawSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/ClawSubsystem.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.common.subsystem;
 
-import android.util.Pair;
-
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
