92a03d9fc8a4e8a713af7297a9e401ea4889be77
Anish Vundela <anishreddyvundela@proton.me>
2024-09-27 21:18:22 +0100

    Add the TelemetryLine class
    This class makes it easier for subsystems to send telemetry to the OpMode for it to handle. Subsystems that have telemetry that we might need will have a getTelemetry() method that returns an array of TelemetryLines.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/TelemetryLine.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.common.util;
+
+public class TelemetryLine {
+    public final String caption;
+    public final String value;
+
+    public TelemetryLine(String caption, String value) {
+        this.caption = caption;
+        this.value = value;
+    }
+}
