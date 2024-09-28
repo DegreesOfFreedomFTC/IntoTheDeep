29b5248aa229e22528afab71beb2e5bc295caf0b
Anish Vundela <anishreddyvundela@proton.me>
2024-09-28 17:59:43 +0100

    Create ForearmLowerCmd and ForearmRaiseCmd
    These will be triggered by gamepad buttons

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/command/intothedeep/ForearmLowerCmd.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.common.command.intothedeep;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.common.subsystem.ForearmSubsystem;
+
+@SuppressWarnings("unused")
+public class ForearmLowerCmd extends CommandBase {
+
+    private final ForearmSubsystem forearmSubsystem;
+
+    public ForearmLowerCmd(ForearmSubsystem forearmSubsystem) {
+        this.forearmSubsystem = forearmSubsystem;
+        addRequirements(forearmSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        this.forearmSubsystem.power(-0.75);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/command/intothedeep/ForearmRaiseCmd.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.common.command.intothedeep;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.common.subsystem.ForearmSubsystem;
+
+@SuppressWarnings("unused")
+public class ForearmRaiseCmd extends CommandBase {
+
+    private final ForearmSubsystem forearmSubsystem;
+
+    public ForearmRaiseCmd(ForearmSubsystem forearmSubsystem) {
+        this.forearmSubsystem = forearmSubsystem;
+        addRequirements(forearmSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        this.forearmSubsystem.power(0.75);
+    }
+}
