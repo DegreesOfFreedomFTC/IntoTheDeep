637e73a11c24dd740e85db85b24b52a01d2e0364
Anish Vundela <anishreddyvundela@proton.me>
2024-10-08 13:37:21 +0100

    Add second motor to forearm to reflect changes in the design

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/ForearmSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/ForearmSubsystem.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.common.subsystem;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.common.util.TelemetryLine;
@@ -11,27 +12,40 @@ import java.util.LinkedList;
 @SuppressWarnings("unused")
 public class ForearmSubsystem extends SubsystemBase {
 
-    private final DcMotor motor;
+    private final DcMotor leftMotor;
+    private final DcMotor rightMotor;
 
     public ForearmSubsystem(final HardwareMap hardwareMap) {
-        this.motor = hardwareMap.get(DcMotor.class, "faMotor");
-        this.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        this.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.leftMotor = hardwareMap.get(DcMotor.class, "faLeftMotor");
+        this.rightMotor = hardwareMap.get(DcMotor.class, "faRightMotor");
+
+        this.leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        this.rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        this.leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        this.leftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        this.rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     public void power(double power) {
-        this.motor.setPower(power);
+        this.leftMotor.setPower(power);
+        this.rightMotor.setPower(power);
     }
 
     public void resetPosition() {
-        this.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        this.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        this.rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     public LinkedList<TelemetryLine> getTelemetry() {
         LinkedList<TelemetryLine> lines = new LinkedList<>();
 
-        lines.add(new TelemetryLine("Forearm Pos", Double.toString(this.motor.getCurrentPosition())));
+        lines.add(new TelemetryLine("Forearm Pos", Double.toString(this.leftMotor.getCurrentPosition())));
 
         return lines;
     }
